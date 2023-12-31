55caa023e522ec864f5c923fcf453ba723dfeff9
dex <19f19551@mec.edu.om>
2023-12-31 17:35:52 +0400

    final one v1

--- app/src/main/java/com/example/bagpack/SignInScreen.java
+++ app/src/main/java/com/example/bagpack/SignInScreen.java
@@ -2,6 +2,7 @@ package com.example.bagpack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,22 +18,24 @@ public class SignInScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_screen);
-
         name = findViewById(R.id.editTextText);
         password = findViewById(R.id.editTextText4);
         Email = findViewById(R.id.editTextText5);
+        signup = findViewById(R.id.button2);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String n = String.valueOf(name);
-                String p = String.valueOf(password);
-                String e = String.valueOf(Email);
-                if (n.isEmpty() && p.isEmpty() && e.isEmpty()){
-                    Toast.makeText(SignInScreen.this, "Enter Valid input", Toast.LENGTH_SHORT).show();
-                }
-                {
-                    Toast.makeText(SignInScreen.this, "Enter Valid input", Toast.LENGTH_SHORT).show();
+                String n = name.getText().toString();  // Get the text from the EditText
+                String p = password.getText().toString();
+                String e = Email.getText().toString();
+
+                if (n.isEmpty() || p.isEmpty() || e.isEmpty()) {
+                    Toast.makeText(SignInScreen.this, "Enter valid input", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(SignInScreen.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(SignInScreen.this, MainActivity.class);
+                    startActivity(intent);
                 }
             }
         });
